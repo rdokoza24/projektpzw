89fa3b660f1c0c8b7e9401779075817988950ddc
rdokoza24 <rdokoza24@unizd.hr>
2025-11-19 02:13:15 +0100

    run.py prilagoden za render

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 from app import create_app
 from dotenv import load_dotenv
+import os
 
+# Učitaj .env varijable
 load_dotenv()
 
+# Kreiraj Flask aplikaciju
 app = create_app()
 
+# Samo za lokalni razvoj
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
